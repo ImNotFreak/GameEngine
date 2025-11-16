f5066509590bc07eaec59a851602b655aaeb7069
alex <rybakov.alexsandr28@gmail.com>
2025-11-16 11:19:43 +0300

    first

---
+++ Engine/Source/Core/Engine.cpp
@@ -0,0 +1,7 @@
+#include "Engine.h"
+#include <iostream>
+using namespace LifeExe;
+LifeExe::Engine::Engine() 
+{
+    std::cout << "INIT IM_NOT_FREAK ENGINE, Version " << version() << std::endl;
+}
