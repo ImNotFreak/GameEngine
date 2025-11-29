eacb97c03a87db9c33bf0927a6a625c5a53b3a96
alex <rybakov.alexsandr28@gmail.com>
2025-11-29 19:56:48 +0300

    Logger v1

--- Engine/Source/Core/Engine.cpp
+++ Engine/Source/Core/Engine.cpp
@@ -1,8 +1,19 @@
 #include "Engine.h"
-#include <iostream>
+#include "Log/Log.h"
+#include <format>
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
 using namespace LifeExe;
 
 Engine::Engine() 
 {
-    std::cout << "INIT IM_NOT_FREAK ENGINE, Version " << version() << std::endl;
+    Log::getInstance().log(LifeExe::LogVerbosity::Display, 
+        std::format("INIT ENGIENE, VERSION {}", version()));
+
+    Log::getInstance().log(LifeExe::LogVerbosity::Error, std::format("ERROR EXAMPLE {}", version()));
+    Log::getInstance().log(LifeExe::LogVerbosity::Fatal, std::format("FATAL EXAMPLE {}", version()));
+    Log::getInstance().log(LifeExe::LogVerbosity::Warning, std::format("WARNING EXAMPLE, {}", version()));
+    Log::getInstance().log(LifeExe::LogVerbosity::NoLogging, std::format("NO LOG, {}", version()));
 }
---
+++ Engine/Source/Core/Utility.h
@@ -0,0 +1,16 @@
+namespace LifeExe
+{
+class NonCopyable
+{
+protected:
+    NonCopyable() = default;
+    ~NonCopyable() = default;
+
+    NonCopyable(const NonCopyable&) = delete;
+    NonCopyable& operator=(const NonCopyable&) = delete;
+
+    NonCopyable(NonCopyable&&) = delete;
+    NonCopyable& operator=(NonCopyable&&) = delete;
+};
+
+}  
